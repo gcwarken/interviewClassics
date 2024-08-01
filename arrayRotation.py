4fd3b745d6ceb3a34b6c6d5e414afe36520fe4e4
Guarajuba <gcwarken@gmail.com>
2024-08-01 18:09:44 -0300

    better array rotation

--- arrayRotation.py
+++ arrayRotation.py
@@ -28,3 +28,21 @@ for i in range(k % n):
 	print("Rotation " + str(i+1) + ": " + str(myArray))
 
 print("Rotated array: " + str(myArray))
+
+########## A better aproach 
+def rightRotation(a, k):
+# rotate array a to the right k times
+
+	rotations = k % len(a)
+	rotatedArray = []
+
+	# get elements after Kth index
+	for i in range(rotations, len(a)):
+		rotatedArray.append(a[i])
+
+	# get elements before Kth index
+	for i in range(0, rotations):
+		rotatedArray.append(a[i])
+
+	print("Original array: " + str(a))
+	print("Rotated array: " + str(rotatedArray))
