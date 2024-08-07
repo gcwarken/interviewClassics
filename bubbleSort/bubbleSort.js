5a3d419887373138ae5a6cc1f9cf736168ffa565
gcwarken <gcwarken@gmail.com>
2024-08-07 16:41:33 -0300

    Bubble sort - javascript

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -1,1 +1,21 @@
 // Bubble Sort algorithm - javascript implementation
+
+function bubbleSort(arr) {
+    let i, j, temp;
+    
+    console.log(`Original array: ${arr}`);
+
+    for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < arr.length - i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    console.log(`Sorted array: ${arr}`);
+}
+
+bubbleSort([5,1,8,2,7,6,3,0,4])
--- bubbleSort/bubbleSort.py
+++ bubbleSort/bubbleSort.py
@@ -2,8 +2,9 @@
 
 def bubbleSort(arr):
     print("Original array: " + str(arr))
+    
     for i in range(len(arr)):
-        for j in range(len(arr) - i - 1):
+        for j in range(len(arr) - i-1):
             # print("Comparing " + str(arr[j]) + " and " + str(arr[j+1]))
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
