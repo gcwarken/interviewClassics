a782b85220d5f9b4a3f9ea6b7aaa721c9a9c588a
gcwarken <gcwarken@gmail.com>
2024-08-13 16:03:23 -0300

    Reverse string words - javascript

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,20 @@
 // Reverse words in a given string - javascript implementation
 
-function reverseStringWords(myString){
-    console.log(myString);
+function reverseStringWords(s){
+    let reversedWords = ""
+    let j = s.length
+    for (i = s.length-1; i >= 0; i--){
+        if (s[i] == " ") {
+            reversedWords += s.slice(i+1,j) + " ";
+            j = i;
+        } else if (i == 0) {
+            reversedWords += s.slice(i,j);
+        }
+    }
+    return reversedWords;
 }
+
+let s = "the words of this sentence are reversed"
+
+console.log(`Original string: ${s}`);
+console.log(`String with words reversed: ${reverseStringWords(s)}`);
