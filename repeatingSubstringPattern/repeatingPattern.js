0d1592eb4b946eb94fb840a23a66b5ed5de608ad
gcwarken <gcwarken@gmail.com>
2024-08-20 14:58:51 -0300

    string repeating patter - js

--- repeatingSubstringPattern/repeatingPattern.js
+++ repeatingSubstringPattern/repeatingPattern.js
@@ -1,1 +1,22 @@
 // Given a string, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+function repPattern(s){
+    let i, j = 0;
+    let intHalf = (s.length - (s.length % 2)) / 2;
+    let subPat = s.slice(0,intHalf);
+
+    for (i = intHalf; i >= 0; i--) {
+        if (s.length % subPat.length == 0) {
+            while (j < s.length && s[j] == subPat[j % subPat.length]) {
+                j++;
+            }
+            if (j == s.length) {return true}
+        }
+        j = 0;
+        subPat = s.slice(0,i);
+    }
+
+    return false;
+}
+
+console.log(repPattern("abcabcabc"));
