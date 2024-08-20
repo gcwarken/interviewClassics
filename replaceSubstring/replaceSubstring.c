51c1b05e1dc4b5a39f6274c434b29a44c31063b9
gcwarken <gcwarken@gmail.com>
2024-08-20 12:35:48 -0300

    find/replace - c

--- replaceSubstring/replaceSubstring.c
+++ replaceSubstring/replaceSubstring.c
@@ -4,6 +4,40 @@
 #include <string.h>
 
 int main() {
+   int i = 0;
+   int j = 0;
+   char originalString[] = "They're taking the hobbits to Isengard.";
+   char find[] = "hobbits";
+   char replace[] = "orcs";
+   char newString[1000];
+   char temp[2];
 
+    temp[1] = '\0';
+
+    while (i < strlen(originalString)) {
+        while ((j < strlen(find)) && (originalString[i+j] == find[j])) {
+            j++;
+        }
+
+        if (j == strlen(find)) {
+            strcat(newString, replace);
+            i += j;
+        }
+        else {
+            temp[0] = originalString[i];
+            strcat(newString, temp);
+            i++;
+        }
+
+        j = 0;
+    }
+
+    strcat(newString, "\0");
+
+    printf("Original string: %s\n", originalString);
+    printf("Find substring: %s\n", find);
+    printf("Replace with: %s\n", replace);
+    printf("New string: %s\n", newString);
+    
     return 0;
 }
