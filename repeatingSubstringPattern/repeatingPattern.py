5c41d0bf81d2a99bd891aed5ecfa077fddd64723
gcwarken <gcwarken@gmail.com>
2024-08-23 17:29:56 -0300

    string repeating pattern - python

--- repeatingSubstringPattern/repeatingPattern.py
+++ repeatingSubstringPattern/repeatingPattern.py
@@ -1,1 +1,18 @@
 # Given a string, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+def repPattern(s):
+    half = len(s) // 2
+    subPat = s[0:half]
+    j = 0
+
+    for i in range(half, 1, -1):
+        if len(s) % len(subPat) == 0:
+            while ( j < len(s) and s[j] == subPat[j % len(subPat)]):
+                j += 1
+            if (j == len(s)): return True
+        j = 0
+        subPat = s[0:i]
+
+    return False
+
+print(repPattern("abcabcabc"))
