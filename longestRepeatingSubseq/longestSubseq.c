b952b437a0af57c02362c40e91a54a14ad8014b6
gcwarken <gcwarken@gmail.com>
2024-08-28 10:11:46 -0300

    Longest repeating subsequence

---
+++ longestRepeatingSubseq/longestSubseq.c
@@ -0,0 +1,12 @@
+// Given a string, find the length of the longest repeating subsequence.
+// A subsequence is a string generated from the original string by deleting 0 or more 
+// characters and without changing the relative order of the remaining characters.
+// Ex.: input "AABEBCDD" => longest repeating subsequence ABD - length 3
+// C implementation
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
