69ec561100ce4096eeb5d843ccf18aa19a7e9e10
gcwarken <gcwarken@gmail.com>
2024-09-03 17:08:58 -0300

    String subsequences - python

--- stringSubsequences/stringSubseq.py
+++ stringSubsequences/stringSubseq.py
@@ -1,2 +1,22 @@
 # Find all string subsequences
 # Python implementation
+
+def getAllSubseq(s):
+    subseqs = []
+    tempString = ""
+
+    for combinations in range( ((2 ** len(s)) - 1) , 0, -1):
+        i = combinations
+        j = 0
+        tempString = bin(combinations) + ": "
+        while (i > 0):
+            if (i & 1):
+                tempString += s[j]
+            j += 1
+            i = i >> 1
+        subseqs.append(tempString)
+
+    return subseqs
+
+myString = "ABCD"
+print(getAllSubseq(myString))
