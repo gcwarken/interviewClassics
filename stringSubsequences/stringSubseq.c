183eee31dc09f398a10fa2975ce69216554b7136
gcwarken <gcwarken@gmail.com>
2024-09-06 13:09:34 -0300

    String subsequences - c

--- stringSubsequences/stringSubseq.c
+++ stringSubsequences/stringSubseq.c
@@ -2,8 +2,25 @@
 // C implementation
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    char myString[] = "ABCD";
+    int combinations = (1 << strlen(myString)) - 1;
+    int i, j;
+
+    for (combinations; combinations > 0; combinations--) {
+        i = combinations;
+        j = 0;
+        while (i > 0) {
+            if (i & 1) {
+                printf("%c", myString[j]);
+            }
+            i = i >> 1;
+            j++;
+        }
+        printf("\n");
+    }
 
     return 0;
 }
