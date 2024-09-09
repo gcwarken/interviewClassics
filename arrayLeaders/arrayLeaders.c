f580214f912de465473bebc7576894a2dfad7850
gcwarken <gcwarken@gmail.com>
2024-09-09 14:55:07 -0300

    Array leaders

---
+++ arrayLeaders/arrayLeaders.c
@@ -0,0 +1,9 @@
+// Print the leaders in an array - C implementation
+// An element is a Leader if it is greater than all the elements to its right side.
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
---
+++ arrayLeaders/arrayLeaders.py
@@ -0,0 +1,2 @@
+# Print the leaders in an array - Python implementation
+# An element is a Leader if it is greater than all the elements to its right side.
